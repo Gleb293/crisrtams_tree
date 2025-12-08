ca354e28cc76840f3b444d616c3ef68613d8d2c5
GymBro01 <gilgleb4@gmail.com>
2025-12-08 18:56:17 +0300

    sffs
    fsfsf

--- scripts/cristmas_tree.js
+++ scripts/cristmas_tree.js
@@ -133,67 +133,230 @@ let toys = [
         shape: "ball", 
         favorite: true, 
         image: "./images/5305.png" 
-    }
+    },{
+    name: "Желтая игрушка с цветком",
+    color: "red", 
+    count: 5, 
+    year: 2023, 
+    shape: "ball", 
+    favorite: true,
+    image: "./images/5294.png" 
+   },
+{ 
+   name: "Красная игрушка с цветком", 
+   color: "red", 
+   count: 5, 
+   year: 2023 ,
+   shape: "ball", 
+   favorite: true, 
+   image: "./images/5306.png" 
+},
+{ 
+   name: "Синяя игрушка с цветком", 
+   color: "blue", 
+   count: 5, 
+   year: 2023 , 
+   shape: "ball", 
+   favorite: true, 
+   image: "./images/5300.png" 
+},
+{ 
+   name: "Черная игрушка с цветком", 
+   color: "black", 
+   count: 5, 
+   year: 2023 , 
+   shape: "ball", 
+   favorite: true, 
+   image: "./images/5305.png" 
+},
+{ 
+   name: "Желтая игрушка с цветком",
+    color: "red", 
+    count: 5, 
+    year: 2023, 
+    shape: "ball", 
+    favorite: true,
+    image: "./images/5297.png" 
+   },
+{ 
+   name: "Красная игрушка с цветком", 
+   color: "red", 
+   count: 5, 
+   year: 2023 ,
+   shape: "ball", 
+   favorite: true, 
+   image: "./images/5297.png" 
+},
+{ 
+   name: "Синяя игрушка с цветком", 
+   color: "blue", 
+   count: 5, 
+   year: 2023 , 
+   shape: "ball", 
+   favorite: true, 
+   image: "./images/5300.png" 
+},
+{ 
+   name: "Черная игрушка с цветком", 
+   color: "black", 
+   count: 5, 
+   year: 2023 , 
+   shape: "ball", 
+   favorite: true, 
+   image: "./images/5305.png" 
+},
 ];
-const toysgrid =document.querySelector(".toes-grid");
-toys.forEach((toy,index)=>{
-const toybox=document.createElement("div");
 
-const img=document.createElement("img");
-img.src=toy.image;
-img.classList.add("toy");
-img.draggable=true;
-img.dataset.index=index;
+let currentTree = {
+    type: "",
+    garland: "",
+    toys: [],
+  
+    setTree(newType) {
+      this.type = newType;
+    },
+  
+    setGarland(newGarland) {
+      this.garland = newGarland;
+    },
 
-const countbox=document.createElement("div");
-countbox.textContent=toy.count;
-countbox.style.color="white";
-countbox.style.textAlign="center";
-countbox.style.fontSize="14px";
-
-toybox.appendChild(img);
-toybox.appendChild(countbox);
-toysgrid.appendChild(toybox);
-img.addEventListener("dragstart",e=>{
-    if(toy.count===0){
-        e.preventDefault();
-        return;
+    addToy(toyObj) {
+      this.toys.push(toyObj);
+    },
+  
+    showInfo() {
+      console.log("Ёлка:", this.type);
+      console.log("Гирлянда:", this.garland);
+      console.log("Игрушки:", this.toys);
     }
-    e.dataTransfer.setData("toy",index);
-}
-);
+};
+
+
+const toysGrid =document.querySelector(".toys-grid");
+
+toys.forEach((toy,index)=>{
+    const toyBox=document.createElement("div");
+
+    const img=document.createElement("img");
+
+    img.src=toy.image;
+    img.classList.add("toy");
+    img.draggable=true;
+    img.dataset.index=index;
+
+    const countBox=document.createElement("div");
+    countBox.textContent=toy.count;
+    countBox.style.color="white";
+    countBox.style.textAlign="center";
+    countBox.style.fontSize="14px";
+
+    toyBox.appendChild(img);
+    toyBox.appendChild(countBox);
+    toysGrid.appendChild(toyBox);
+
+    img.addEventListener("dragstart",e=>{
+        if(toy.count===0){
+            e.preventDefault();
+            return;
+        }
+        e.dataTransfer.setData("toy",index);
+    }
+    );
 
 });
-const treearea=document.querySelector(".tree-area");
-treearea.addEventListener("drop",e=>{e.preventDefault});
-const rect=treearea.getBoundingClientRect();
-const x=e.clientX-rect.left;
-const y=e.clientY-rect.top;
-if(e.dataTransfer.getData("toy")!==""){
-const toyindex=e.dataTransfer.getData("toy");
-const toy=toys[toyindex];
-if (toy.count>0){
-    toy.count-=1;
-    const xpos=x-40;
-    const ypos=y-40;
-    const img=document.createElement("img");
-    img.src=toy.image;
-    img.classList.add("toyomtree");
-    img.style.left=xpos+"px";
-    img.style.top=ypos+"px";
-    treearea.appendChild(img);
-    currenttree.addtoy(toy,xpos,ypos);
-    img.addEventListener("click",()=>{
+
+
+let placedCounter = 0;
+
+const treeArea = document.querySelector(".tree-area");
+
+treeArea.addEventListener("dragover", e => e.preventDefault());
+
+treeArea.addEventListener("drop", e => {
+  e.preventDefault();
+
+  const rect = treeArea.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  if (e.dataTransfer.getData("toy") !== "") {
+    const toyIndex = e.dataTransfer.getData("toy");
+    const toy = toys[toyIndex];
+
+    if (toy.count > 0) {
+      toy.count--;
+
+      const xPos = x - 40;
+      const yPos = y - 40;
+
+      placedCounter++;
+      const placedId = placedCounter;
+
+      const img = document.createElement("img");
+      img.src = toy.image;
+      img.classList.add("toy-on-tree");
+
+      img.style.left = xPos + "px";
+      img.style.top = yPos + "px";
+
+      // сохраняем id в DOM
+      img.dataset.placedId = placedId;
+
+      treeArea.appendChild(img);
+
+      // сохраняем в объект текущей ёлки
+      currentTree.addToy({
+        id: toy.id,
+        placedId: placedId,
+        x: xPos,
+        y: yPos,
+        image: toy.image
+      });
+      console.log('currentTree.toys:', currentTree.toys);
+      
+
+      toysGrid.children[toyIndex].children[1].textContent = toy.count;
+
+      img.addEventListener("click", () => {
+        console.log("Клик по игрушке", img.dataset.placedId);
+
         img.remove();
-        toy.count+=1;
-    toysgrid.children[toyindex].children[1].textContent=toy.count;
-        currenttree.toys=currenttree.toys.filter(t=>t.id!==toy.id);
-    });
+
+        toy.count++;
+        toysGrid.children[toyIndex].children[1].textContent = toy.count;
+
+        currentTree.toys = currentTree.toys.filter(
+          t => t.placedId != img.dataset.placedId // удалить игрушку
+        );
+      });
+    }
+  }
+
+  if (e.dataTransfer.getData("garland") !== "") {
+    const gIndex = e.dataTransfer.getData("garland");
+    const garland = garlands[gIndex];
+
+    currentTree.setGarland(garland.type);
+
+    const img = document.createElement("img");
+    img.src = garland.image;
+    img.classList.add("garland-on-tree");
+
+    img.style.left = (x - 140) + "px";
+    img.style.top = (y - 20) + "px";
+
+    img.style.animationDelay = (Math.random() * 1.6) + "s";
+
+    treeArea.appendChild(img);
     
-
-}
-
-};
+    //при клике на гирлянду удаляем ее из DOM
+     img.addEventListener("click", () => {
+      img.remove();
+      // удаляем гирлянду из объекта текущей ёлки
+      currentTree.setGarland("");
+    });
+  }
+});
 
 
 
